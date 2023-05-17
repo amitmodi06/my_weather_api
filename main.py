0e90d17c1019fe4787f4655200547fed3d6042e8
amitmodi06 <amitmodi06@gmail.com>
2023-05-17 22:54:47 +0100

    Initial commit relevant to the project and removed unwanted files

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
 
 # create a website object
-my_app = Flask("Website")
+my_app = Flask(__name__)
 
 
-@my_app.route("/home")  # @ shows that this line is a decorator
+@my_app.route("/")  # @ shows that this line is a decorator
 def home_page():
     # return_template function is needed to return html file
     return render_template("home.html")
 
-@my_app.route("/about") # @ shows that this line is a decorator
-def about_page():
-    # return_template function is needed to return html file
-    return render_template("about.html")
 
-my_app.run(debug=True)
+@my_app.route("/api/v1/<station>/<date>")  # @ shows that this line is a decorator
+def about_page(station, date):
+    temperature = 54
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__main__":
+    my_app.run(debug=True)
